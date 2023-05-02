19bddd153831ce46bf04289896aea475bdf6ce0f
Ali Iqbal <ali.iqbal@invozone.com>
2023-05-02 13:14:07 +0500

    port added in env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './file.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3009);
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
