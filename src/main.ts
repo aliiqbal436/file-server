dc1e0b37d92b43209d0771ea95de2870bf904077
Ali Iqbal <ali.iqbal@invozone.com>
2023-05-12 10:58:36 +0500

    fix cors issue

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,24 @@ import * as fs from 'fs';
 import * as https from 'https';
 import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
 import { NestApplicationOptions } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  // const httpsOptions: HttpsOptions = {
-  //   key: fs.readFileSync(process.env.CERTKEYPATH),
-  //   cert: fs.readFileSync(process.env.CERTCRTPATH),
-  // };
-  // const server = https.createServer(httpsOptions);
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  };
+  const httpsOptions: HttpsOptions = {
+    key: fs.readFileSync(process.env.CERTKEYPATH),
+    cert: fs.readFileSync(process.env.CERTCRTPATH),
+  };
+  const server = https.createServer(httpsOptions);
   const app = await NestFactory.create(AppModule, {
-    // httpsOptions: server,
+    httpsOptions: server,
+    cors: true,
   } as NestApplicationOptions);
+
+  app.enableCors(corsOptions);
 
   await app.listen(process.env.APP_PORT);
 }
